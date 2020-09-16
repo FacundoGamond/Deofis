be9cc23f7f5a036d0e38b0d5ad857a0ab99691d5
Facundo Gamond <facugamond@hotmail.com>
2020-09-16 01:35:44 -0300

    Se agrego funcionalidad y loal storage al componente oferts

--- src/app/components/oferts/oferts.component.ts
+++ src/app/components/oferts/oferts.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./oferts.component.scss']
 })
 export class OfertsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public images
+  public close: boolean
+  constructor() {
+    this.close = false;
   }
 
+  ngOnInit(): void {
+    this.images = [
+      { path: '../../assets/images/1.jpg' },
+      { path: '../../assets/images/2.jpg' },
+      { path: '../../assets/images/3.jpg' },
+      { path: '../../assets/images/4.jpg' },
+      { path: '../../assets/images/5.jpg' },
+    ]
+
+    if (localStorage.getItem("ofert") == "true") {
+      this.close = true
+    }
+  }
+
+  closeOfert() {
+    this.close = true
+    localStorage.setItem("ofert", "true")
+  }
 }
