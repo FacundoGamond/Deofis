3f03541240f3f8b568e90e6a31a7dbd561e05278
Facundo Gamond <facugamond@hotmail.com>
2020-09-16 11:07:17 -0300

    Se maquetaron el sidebar y products, ademas se agrego el modelo de productos

---
+++ src/app/models/project.ts
@@ -0,0 +1,9 @@
+export class Project {
+    constructor(
+        public _id: number,
+        public nombre: string,
+        public description: string,
+        public precio: number,
+        public marca: string
+    ) { }
+}
